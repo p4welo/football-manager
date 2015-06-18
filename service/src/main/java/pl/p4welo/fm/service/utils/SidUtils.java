0883df2d85b3eb4f757807bc1c21c93e8dccf10b
p4welo <p4welo@gmail.com>
2015-06-18 13:20:20 +0200

    webjars

---
+++ service/src/main/java/pl/p4welo/fm/service/utils/SidUtils.java
@@ -0,0 +1,14 @@
+package pl.p4welo.fm.service.utils;
+
+import java.util.UUID;
+
+/**
+ * User: pawel.radomski
+ * Date: 12.04.13
+ * Time: 15:03
+ */
+public class SidUtils {
+    public static final String generate() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
--- web/src/main/java/pl/p4welo/fm/web/config/Application.java
+++ web/src/main/java/pl/p4welo/fm/web/config/Application.java
@@ -2,6 +2,8 @@ package pl.p4welo.fm.web.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +11,20 @@ import org.springframework.context.annotation.Configuration;
  * Created by parado on 2015-06-17.
  */
 @Configuration
-@ComponentScan("pl.p4welo.fm.web")
+@ComponentScan({
+        "pl.p4welo.fm.web",
+        "pl.p4welo.fm.service",
+        "pl.p4welo.fm.dao"
+})
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
